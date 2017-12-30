9a7a95c90a8b94fbf3d5e2f8e648f58d4eb126a4
Rod Ta <rody101@gmail.com>
2017-12-30 00:56:18 -0500

    minor fixes, changed url

--- RST/turk/urls.py
+++ RST/turk/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^reservation/(?P<stop_id>[0-9]+)/train/(?P<train_id>[0-9]+)/seg/(?P<seg_id>[0-9]+)/des/(?P<des_id>[0-9]+)/(?P<date>\d{4}-\d{2}-\d{2})/$', views.reservation, name='reservation'),
     url(r'^cancellation/$', views.cancellation, name='cancellation'),
     #url(r'^rebook/$', views.rebook, name='rebook'),
-    #url(r'^createdb/$', views.createdb, name='createdb'),
+    url(r'^createdb/$', views.createdb, name='createdb'),
     url(r'^confirm_cancellation/$', views.confirm_cancellation, name='confirm_cancellation'),
     url(r'^confirm_reservation/pass/(?P<pass_id>[0-9]+)/res/(?P<res_id>[0-9]+)/stop/(?P<stop_id>[0-9]+)/trip/(?P<trip_id>[0-9]+)/$', views.confirm_reservation, name='confirm_reservation'),
 
